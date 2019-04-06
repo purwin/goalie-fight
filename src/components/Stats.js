0c3bafe111afffdfb30993212ae8f11ce015ac98
Michael Purwin <mpurwin@gmail.com>
2019-04-06 14:29:16 -0400

    Init Stats component

---
+++ src/components/Stats.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Stats extends Component {
+  render() {
+    return (
+      <div className="box">
+        <div className="box-head">
+          <h3>AVERAGES</h3>
+        </div>
+        <div className="box-body">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Stats;
