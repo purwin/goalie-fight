e34a9f2707579a35c16455cddd12eb0b54491fb5
Michael Purwin <mpurwin@gmail.com>
2019-04-06 14:28:30 -0400

    Add Header, Chart to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header';
+import Chart from './components/Chart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
+      <Chart />
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
