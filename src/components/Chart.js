d03a491d7a53265376c9c238ca74537f01590f51
Michael Purwin <mpurwin@gmail.com>
2019-04-06 14:27:41 -0400

    Init Chart component

---
+++ src/components/Chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Chart extends Component {
+  render() {
+
+    return(
+      <div className="chart box">
+        <img src="http://www.cs.middlebury.edu/~candrews/showcase/infovis_techniques_s16/radar_chart/chart.png" alt=""/>
+      </div>
+    );
+  }
+}
+
+export default Chart;
