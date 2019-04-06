884cddd1dfbbb4468d4eb654e78520d1a5a3b84e
Michael Purwin <mpurwin@gmail.com>
2019-04-06 14:27:58 -0400

    Init Header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div>
+      <h1>Goalie Fight</h1>
+    </div>
+    <div>
+      <nav>
+        <ol>
+          <li>ABOUT</li>
+          <li>GLOSSARY</li>
+          <li>RESOURCES</li>
+        </ol>
+      </nav>
+    </div>
+  </header>
+);
+
+export default Header;
