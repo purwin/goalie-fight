d649b5e16fb219e840ee046f26bcbce261595eb3
Michael Purwin <mpurwin@gmail.com>
2019-04-07 11:39:57 -0400

    Change to a stateless component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Stats extends Component {
-  render() {
+const Stats = ({stats}) => {
+  console.log(Object.values(stats));
     return (
       <div className="box">
         <div className="box-head">
           <h3>AVERAGES</h3>
         </div>
         <div className="box-body">
+          <ul>
+            {Object.values(stats).map((stat, index) => (
+              <li key={index}>{stat}</li>
+            ))}
+          </ul>
         </div>
       </div>
     );
-  }
 }
 
 export default Stats;
