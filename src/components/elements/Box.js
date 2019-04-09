8c99d3dfd7fdf78dc4c59d40ce2e304146a1eabb
Michael Purwin <mpurwin@gmail.com>
2019-04-08 22:45:56 -0400

    Init Box component

---
+++ src/components/elements/Box.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import React from 'react'
+
+const Box = styled.div`
+width: 30%;
+margin: auto;
+background: #021926;
+color: #dfe6e9;
+`;
+
+const Head = styled.h3`
+`;
+
+export default ({title, children}) => (
+  <Box>
+    <Head>
+      {title}
+    </Head>
+    {children}
+  </Box>
+)
