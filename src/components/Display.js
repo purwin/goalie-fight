d884e99e8b541323055788987d092e6893a0e8b1
Michael Purwin <mpurwin@gmail.com>
2019-04-09 20:42:50 -0400

    Add goalies, goalieList, time, situation props

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from 'react'
 
-import Comparables from './Comparables';
-import Stats from './Stats';
-import GoalieView from './GoalieView';
+import Comparables from './Comparables'
+import Stats from './Stats'
+import GoalieView from './GoalieView'
 
-const Display = ({stats}) => {
+const Display = ({stats, goalies, goalieList, time, situation}) => {
 
-    return(
-      <>
-        {/* <Stats
-          stats={stats}
-        /> */}
-        <GoalieView
-          stats={stats}
-        />
-        {/* <Comparables /> */}
-      </>
-    );
-}
+  return(
+    <>
+      {/* <Stats
+        stats={stats}
+      /> */}
+      <GoalieView
+        stats={stats}
+        goalies={goalies}
+        goalieList={goalieList}
+        time={time}
+        situation={situation}
+      />
+      {/* <Comparables /> */}
+    </>
+  )
+};
 
-export default Display;
+export default Display
