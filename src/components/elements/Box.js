2c3bd81a5338b6528eb4c3e872f4af5c74607d34
Michael Purwin <mpurwin@gmail.com>
2019-04-09 20:45:43 -0400

    Clean up formatting

--- src/components/elements/Box.js
+++ src/components/elements/Box.js
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 import React from 'react'
 
 const Box = styled.div`
-height: 50vh;
-width: 30%;
-margin: auto;
-background: #021926;
-color: #dfe6e9;
+	height: 50vh;
+	width: 30%;
+	margin: auto;
+	background: #021926;
+	color: #dfe6e9;
 `;
 
 const Head = styled.h3`
