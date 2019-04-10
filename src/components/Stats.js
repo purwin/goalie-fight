f8af4a3bd4cf34623d41d14957af198894524575
Michael Purwin <mpurwin@gmail.com>
2019-04-09 20:41:28 -0400

    Clean up formatting

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from 'react'
 
-import Box from './elements/Box';
+import Box from './elements/Box'
 
 const Stats = ({stats}) => {
   console.log(Object.values(stats));
-    return (
-      <Box title={`Averages`}>
-        <div className="box-body">
-          <ul>
-            {Object.values(stats).map((stat, index) => (
-              <li key={index}>{stat}</li>
-            ))}
-          </ul>
-        </div>
-      </Box>
-    );
-}
 
-export default Stats;
+  return(
+    <Box title={`Averages`}>
+      <div className="box-body">
+        <ul>
+          {Object.values(stats).map((stat, index) => (
+            <li key={index}>{stat}</li>
+          ))}
+        </ul>
+      </div>
+    </Box>
+  );
+};
+
+export default Stats
