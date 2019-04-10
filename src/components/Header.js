21ecc84ae3c157efd09313a98b6ff93b27a68535
Michael Purwin <mpurwin@gmail.com>
2019-04-09 20:44:08 -0400

    Create HeaderBar, Nav style components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: space-between;
+  height: 15vh;
+  background: papayawhip;
+  color: palevioletred;
+  padding: 1rem;
+`;
+
+const Nav = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+
+  & li:hover {
+    color: tomato;
+  }
+`;
 
 const Header = () => (
-  <header>
-    <div>
-      <h1>Goalie Fight</h1>
-    </div>
-    <div>
-      <nav>
-        <ul>
-          <li>ABOUT</li>
-          <li>GLOSSARY</li>
-          <li>RESOURCES</li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <HeaderBar>
+    <h1>Goalie Fight</h1>
+    <Nav>
+      <li>ABOUT</li>
+      <li>GLOSSARY</li>
+      <li>RESOURCES</li>
+    </Nav>
+  </HeaderBar>
 );
 
 export default Header;
