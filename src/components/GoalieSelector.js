f5bc302da9f2d458b74f0a728fae46197ccc77d0
Michael Purwin <mpurwin@gmail.com>
2019-04-10 23:21:04 -0400

    Create onChangeGoalie function

--- src/components/GoalieSelector.js
+++ src/components/GoalieSelector.js
@@ -1,20 +1,30 @@
 import React from 'react'
 
-const GoalieSelector = ({goalie, goalieList}) => {
+const GoalieSelector = ({index, goalie, goalieList, changeGoalie, addGoalie, pullGoalie}) => {
+
+	const onChangeGoalie = (option) => {
+    console.log(option.target.options[option.target.selectedIndex].text);
+    console.log(option.target.value);
+		// changeGoalie()
+	}
 	return(
 		<div>
-			<select>
+			<select
+				value={goalie.id}
+				onChange={onChangeGoalie}
+			>
 				<option value="">Select a Goalie!</option>
 				{goalieList.map(goalieOption => (
 					<option
 						key={goalieOption.id}
 						value={goalieOption.id}
-						selected={goalie.id === goalieOption.id}
+						goalie={goalieOption.name}
 					>
 						{goalieOption.name}
 					</option>
 				))}
 			</select>
+			{index > 0 ? <button onClick={addGoalie}>X</button> : <button onClick={() => pullGoalie(index)}>+</button>}
 		</div>
 	)
 };
