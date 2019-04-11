e28ba00120a25b374349ba40628a62cfb65a9304
Michael Purwin <mpurwin@gmail.com>
2019-04-10 23:20:21 -0400

    Add styled div component, propTypes

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-import Comparables from './Comparables'
-import Stats from './Stats'
+// import Comparables from './Comparables'
+// import Stats from './Stats'
 import GoalieView from './GoalieView'
 
-const Display = ({stats, goalies, goalieList, time, situation}) => {
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, minmax(500px, 1fr));
+  background: mintcream;
+  color: cadetblue;
+`;
+
+const Display = ({stats, goalies, goalieList, time, situation, changeGoalie, addGoalie, pullGoalie}) => {
 
   return(
-    <>
+    <MainContainer>
       {/* <Stats
         stats={stats}
       /> */}
@@ -17,10 +26,24 @@ const Display = ({stats, goalies, goalieList, time, situation}) => {
         goalieList={goalieList}
         time={time}
         situation={situation}
+        changeGoalie={changeGoalie}
+        addGoalie={addGoalie}
+        pullGoalie={pullGoalie}
       />
       {/* <Comparables /> */}
-    </>
+    </MainContainer>
   )
 };
+
+Display.propTypes = {
+  stats: PropTypes.array,
+  goalies: PropTypes.array,
+  goalieList: PropTypes.array,
+  time: PropTypes.string,
+  situation: PropTypes.string,
+  changeGoalie: PropTypes.func,
+  addGoalie: PropTypes.func,
+  pullGoalie: PropTypes.func,
+}
 
 export default Display
