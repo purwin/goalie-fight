44a9cda902391fb89dd3ea4bacf17137ff22b106
Michael Purwin <mpurwin@gmail.com>
2019-04-16 21:59:57 -0400

    Init maths.js

---
+++ src/utils/maths.js
@@ -0,0 +1,12 @@
+
+
+// Function that calculates percentile of a given number
+// Receives a number and an array as arguments
+// Returns percentile as number
+const percentile = (num, arr) => {
+	// Get array of values < given number
+	const position = arr.filter(item => item < num);
+
+	// Calculate percentile
+	return (position.length / arr.length) * 100
+};
