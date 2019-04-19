ee369c0195f43d23e5d33d03b3b7624219407f50
Michael Purwin <mpurwin@gmail.com>
2019-04-18 23:32:02 -0400

    Add duplicates count to percentile calc

--- src/utils/maths.js
+++ src/utils/maths.js
@@ -5,6 +5,9 @@ export const percentile = (num, arr) => {
 	// Get array of values < given number
 	const position = arr.filter(item => item < num);
 
+	// Get count of numbers with same rank as given num
+	const duplicates = arr.filter( item => item === num);
+
 	// Calculate percentile
-	return (position.length / arr.length) * 100
+	return (position.length + (.5 * duplicates.length)) / arr.length * 100
 };
