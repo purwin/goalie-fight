ac2be25012e064954edeb7b45d21dfa4dd8660c2
Michael Purwin <mpurwin@gmail.com>
2019-04-21 23:13:35 -0400

    Add 100% width, add conditional to Head component

--- src/components/elements/Box.js
+++ src/components/elements/Box.js
@@ -2,9 +2,8 @@ import styled from 'styled-components'
 import React from 'react'
 
 const Box = styled.div`
-	height: 50vh;
-	width: 30%;
-	margin: auto;
+  height: 50vh;
+	width: 100%;
 	background: #021926;
 	color: #dfe6e9;
 `;
@@ -14,9 +13,7 @@ const Head = styled.h3`
 
 export default ({title, children, className}) => (
   <Box className={className}>
-    <Head>
-      {title}
-    </Head>
+    {title && <Head>{title}</Head>}
     {children}
   </Box>
 )
