11cd4599ab9794effbf8e6a6f6fdba6fed3db47e
Michael Purwin <mpurwin@gmail.com>
2019-04-22 10:40:06 -0400

    Style stats div, add more table values

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,10 +3,21 @@ import styled from 'styled-components'
 
 import Box from './elements/Box'
 
+const GoalieName = styled.h3`
+  text-align: left;
+  font-size: 1.5rem;
+  margin: .5rem 0;
+`;
+
 const StatTable = styled.table`
   width: 100%;
-  padding: .5rem;
   text-align: left;
+`;
+
+const GoalieStat = styled.div`
+  margin: 1rem;
+  padding: 0 0 1rem 0;
+  border-bottom: 3px solid #80B8FF;
 `;
 
 const Stats = ({stats}) => {
@@ -18,8 +29,8 @@ const Stats = ({stats}) => {
       {stats.map(goalie => {
         if (goalie.name) {
           return(
-            <div>
-              <h3>{goalie.name} ({goalie.team})</h3>
+            <GoalieStat>
+              <GoalieName>{goalie.name} ({goalie.team})</GoalieName>
               <StatTable>
                 <thead>
                   <tr>
@@ -30,13 +41,28 @@ const Stats = ({stats}) => {
                 </thead>
                 <tbody>
                   <tr>
+                    <td>GP</td>
+                    <td>{goalie.gp}</td>
+                    <td>{goalie.p_gp.toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>TOI</td>
+                    <td>{goalie.toi.toFixed(2)}</td>
+                    <td>{goalie.p_toi.toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>SHOTS</td>
+                    <td>{goalie.sa}</td>
+                    <td>{goalie.p_sa.toFixed(2)}</td>
+                  </tr>
+                  <tr>
                     <td>SV%</td>
                     <td>{goalie.sv.toFixed(3)}</td>
-                    <td>{goalie.p_sv}</td>
+                    <td>{goalie.p_sv.toFixed(2)}</td>
                   </tr>
                 </tbody>
               </StatTable>
-            </div>
+            </GoalieStat>
           )
         }
 
