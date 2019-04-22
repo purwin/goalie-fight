1e817115e0e4d02ad027b05fc2e716c0ddea4cf6
Michael Purwin <mpurwin@gmail.com>
2019-04-22 09:44:00 -0400

    Add map func to display stat table

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,18 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Box from './elements/Box'
 
+const StatTable = styled.table`
+  width: 100%;
+  padding: .5rem;
+  text-align: left;
+`;
+
 const Stats = ({stats}) => {
-  console.log(Object.values(stats));
+  console.log(stats);
 
   return(
-    <Box title={`Averages`}>
+    <Box title={`Stats`}>
       <div className="box-body">
-        <ul>
-          {Object.values(stats).map((stat, index) => (
-            <li key={index}>{stat}</li>
-          ))}
-        </ul>
+      {stats.map(goalie => {
+        if (goalie.name) {
+          return(
+            <div>
+              <h3>{goalie.name} ({goalie.team})</h3>
+              <StatTable>
+                <thead>
+                  <tr>
+                    <td></td>
+                    <td>Value</td>
+                    <td>Rank</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>SV%</td>
+                    <td>{goalie.sv.toFixed(3)}</td>
+                    <td>{goalie.p_sv}</td>
+                  </tr>
+                </tbody>
+              </StatTable>
+            </div>
+          )
+        }
+
+        return false;
+      })}
       </div>
     </Box>
   );
