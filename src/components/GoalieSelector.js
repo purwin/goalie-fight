d6689b79fa50e68d8a96bc4667cfea90f534a2b8
Michael Purwin <mpurwin@gmail.com>
2019-04-21 23:12:24 -0400

    Create styled div, button components

--- src/components/GoalieSelector.js
+++ src/components/GoalieSelector.js
@@ -1,6 +1,35 @@
 import React from 'react'
 import Select from 'react-select';
 import { ID } from 'postcss-selector-parser';
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const SelectDiv = styled.div`
+	display: grid;
+	grid-template-columns: 1fr auto;
+	grid-gap: 1rem;
+	margin-bottom: .5rem;
+`;
+
+const Selector = styled.button`
+	height: 38px;
+	width: 38px;
+	border: none;
+	border-radius: 50%;
+	background: #F5F9FF;
+	color: #004FB2;
+	margin: 0 0 2.5px;
+	font-size: 1rem;
+
+	&:active {
+		margin: 2.5px 0 0 0;
+	}
+`;
+
+const SelectorAdd = styled(Selector)`
+	background: #004FB2;
+	color: #F5F9FF;
+`;
 
 const GoalieSelector = ({index, goalie, goalieList, changeGoalie, addGoalie, pullGoalie}) => {
 	// Set select options
@@ -21,14 +50,14 @@ const GoalieSelector = ({index, goalie, goalieList, changeGoalie, addGoalie, pul
 	}
 
 	return(
-		<div>
+		<SelectDiv>
 			<Select
         value={goalie.id}
         onChange={onChangeGoalie}
-        options={options}
+				options={options}
       />
-			{index > 0 ? <button onClick={() => pullGoalie(index)}>X</button> : <button onClick={addGoalie}>+</button>}
-		</div>
+			{index > 0 ? <Selector onClick={() => pullGoalie(index)}><FontAwesomeIcon icon="times" /></Selector> : <SelectorAdd onClick={addGoalie}><FontAwesomeIcon icon="plus" /></SelectorAdd>}
+		</SelectDiv>
 	)
 };
 
