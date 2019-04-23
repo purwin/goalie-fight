a491b063b4dc62d2a8265a3493ed6f20eae59464
Michael Purwin <mpurwin@gmail.com>
2019-04-22 22:01:09 -0400

    Create styled Head component, style Head h3

--- src/components/elements/Box.js
+++ src/components/elements/Box.js
@@ -4,16 +4,49 @@ import React from 'react'
 const Box = styled.div`
   height: 50vh;
 	width: 100%;
-	background: #021926;
-	color: #dfe6e9;
+	background: #E5F1FF;
+	color: #001F4C;
 `;
 
-const Head = styled.h3`
+const Head = styled.div`
+  background: #021926;
+  color: #dfe6e9;
+  margin: 0;
+  padding: 1rem;
+`;
+
+const HeadTitle = styled.h3`
+  margin: 0;
+  overflow: hidden;
+  text-align: center;
+
+  &:before,
+  &:after {
+    background: #000;
+    border-top: 2px solid #dfe6e9;
+    border-bottom: 2px solid #dfe6e9;
+    content: "";
+    display: inline-block;
+    height: 5px;
+    position: relative;
+    vertical-align: middle;
+    width: 50%;
+  }
+
+  &:before {
+    right: 0.5em;
+    margin-left: -50%;
+  }
+
+  &:after {
+    left: 0.5em;
+    margin-right: -50%;
+  }
 `;
 
 export default ({title, children, className}) => (
   <Box className={className}>
-    {title && <Head>{title}</Head>}
+    {title && <Head><HeadTitle>{title}</HeadTitle></Head>}
     {children}
   </Box>
 )
