5a73375746257dc946f0143734c2e914707978f3
Michael Purwin <mpurwin@gmail.com>
2019-04-28 10:28:58 -0400

    Update HeadTitle to grid display

--- src/components/elements/Box.js
+++ src/components/elements/Box.js
@@ -17,31 +17,20 @@ const Head = styled.div`
 
 const HeadTitle = styled.h3`
   margin: 0;
-  overflow: hidden;
-  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  grid-gap: 1rem;
+  align-items: center;
 
   &:before,
   &:after {
-    background: #000;
-    border-top: 2px solid #dfe6e9;
-    border-bottom: 2px solid #dfe6e9;
+    display: block;
     content: "";
-    display: inline-block;
-    height: 5px;
-    position: relative;
-    vertical-align: middle;
-    width: 50%;
+    height: 6px;
+    border-top: 3px solid #dfe6e9;
+    border-bottom: 3px solid #dfe6e9;
   }
 
-  &:before {
-    right: 0.5em;
-    margin-left: -50%;
-  }
-
-  &:after {
-    left: 0.5em;
-    margin-right: -50%;
-  }
 `;
 
 export default ({title, children, className}) => (
