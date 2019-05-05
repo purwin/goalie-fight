236b26418a7450531fcebfe9527fe486ccaf73bf
Michael Purwin <mpurwin@gmail.com>
2019-05-05 17:24:14 -0400

    Add 5v5, 4v5 stats, set ID to match goalie IDs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,7 @@
 import * as firebase from 'firebase'
-import * as datum from '../data/stats_2018_5v5-03.json'
+import * as datumAll from '../data/ALL/stats_2018_ALL.json'
+import * as datum5v5 from '../data/5v5/stats_2018_5v5.json'
+import * as datum4v5 from '../data/4v5/stats_2018_4v5.json'
 
 const config = {
   apiKey: "AIzaSyAYtsSeh9Wx699FbJ3E_cen4P86XKjU_hA",
@@ -15,17 +17,37 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 // Loop over data, populate arrays
-datum.default.forEach((item, i) => {
+datumAll.default.forEach((item, i) => {
   // Add goalie name, id to list array
-  database.ref(`2018/5v5/goalies/${i}_${item.team.toLowerCase()}`)
+  database.ref(`2018/ALL/goalies/${item.id}_${item.team.toLowerCase()}`)
     .set({
-      id: i,
+      id: item.id,
       ...item
     });
   
-    database.ref(`2018/list/${i}_${item.team.toLowerCase()}`).set({
-      id: i,
+    database.ref(`2018/options/${item.id}_${item.team.toLowerCase()}`).set({
+      id: item.id,
       name: item.name,
       team: item.team
+    });
+});
+
+// Loop over data, populate arrays
+datum5v5.default.forEach((item, i) => {
+  // Add goalie name, id to list array
+  database.ref(`2018/5v5/goalies/${item.id}_${item.team.toLowerCase()}`)
+    .set({
+      id: item.id,
+      ...item
+    });
+});
+
+// Loop over data, populate arrays
+datum4v5.default.forEach((item, i) => {
+  // Add goalie name, id to list array
+  database.ref(`2018/4v5/goalies/${item.id}_${item.team.toLowerCase()}`)
+    .set({
+      id: item.id,
+      ...item
     });
 });
