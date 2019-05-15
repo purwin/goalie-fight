72552745b8ff9e466a2fa3d4f1fe0193c9fd2f86
Michael Purwin <mpurwin@gmail.com>
2019-05-14 23:11:26 -0400

    Reorder Box elements on small screens

--- src/components/elements/Box.js
+++ src/components/elements/Box.js
@@ -2,10 +2,17 @@ import styled from 'styled-components'
 import React from 'react'
 
 const Box = styled.div`
-  height: 50vh;
+  min-height: 50vh;
 	width: 100%;
 	background: #E5F1FF;
-	color: #001F4C;
+  color: #001F4C;
+
+  @media screen and (max-width: 767.98px) {
+
+    &:nth-of-type(1) {
+      order: 1;
+    }
+  }
 `;
 
 const Head = styled.div`
@@ -13,6 +20,7 @@ const Head = styled.div`
   color: #dfe6e9;
   margin: 0;
   padding: 1rem;
+  border-bottom: 2px solid #021926;
 `;
 
 const HeadTitle = styled.h3`
@@ -21,12 +29,13 @@ const HeadTitle = styled.h3`
   grid-template-columns: 1fr auto 1fr;
   grid-gap: 1rem;
   align-items: center;
+  font-size: 1.5rem;
 
   &:before,
   &:after {
     display: block;
     content: "";
-    height: 6px;
+    height: 8px;
     border-top: 3px solid #dfe6e9;
     border-bottom: 3px solid #dfe6e9;
   }
