ea44d93fde8fb3b2c002e1c9e8a427da3286f373
Michael Purwin <mpurwin@gmail.com>
2019-05-31 22:20:15 -0400

    Add GoalieName hover css

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { darken } from 'polished'
 import AnimateHeight from 'react-animate-height';
 
 import Box from './elements/Box'
 import StatsTable from './elements/StatsTable'
+
+const StatsBox = styled(Box)`
+  height: 100%;
+`;
 
 const GoalieStat = styled.div`
   margin: 0 1rem;
@@ -17,10 +22,14 @@ const GoalieName = styled.h3`
   margin: .5rem 0;
   color: ${props => props.color || 'inherit'}
   cursor: pointer;
+
+  &:hover {
+    color: ${props => darken(0.05, props.color) || 'inherit'}
+  }
 `;
 
 const BoxBody = styled.div`
-  margin: 1rem 0;
+  margin: 0 0 1rem 0;
   background: #E5F1FF;
   overflow: scroll;
 `;
@@ -33,37 +42,35 @@ const Stats = ({stats, rankTotal, activeGoalie, setActiveGoalie, colors, ...prop
   }
 
   return(
-    <Box title={`STATS`}>
+    <StatsBox title={`STATS`}>
       <BoxBody>
-      {stats.map((goalie, i) => {
-        if (goalie.name) {
-          return(
-            <GoalieStat key={`stat_${i}`}>
-              <GoalieName
-                color={colors[i % colors.length]}
-                onClick={() => onSelectGoalie(i)}
-              >
-                {goalie.name} ({goalie.team})
-              </GoalieName>
-              <AnimateHeight
-                duration={300}
-                height={i === activeGoalie ? 'auto' : 0 }
-              >
-                <div>
-                  <StatsTable
-                    goalie={goalie}
-                    rankTotal={rankTotal}
-                  />
-                </div>
-              </AnimateHeight>
-            </GoalieStat>
-          )
-        }
-
-        return false;
-      })}
+          {stats.map((goalie, i) => {
+            if (goalie.name) {
+              return(
+                <GoalieStat key={`stat_${i}`}>
+                  <GoalieName
+                    color={colors[i % colors.length]}
+                    onClick={() => onSelectGoalie(i)}
+                  >
+                    {goalie.name} ({goalie.team})
+                  </GoalieName>
+                  <AnimateHeight
+                    duration={300}
+                    height={i === activeGoalie ? 'auto' : 0 }
+                  >
+                    <StatsTable
+                      goalie={goalie}
+                      rankTotal={rankTotal}
+                    />
+                  </AnimateHeight>
+                </GoalieStat>
+              )
+            }
+  
+            return false;
+          })}
       </BoxBody>
-    </Box>
+    </StatsBox>
   );
 };
 
